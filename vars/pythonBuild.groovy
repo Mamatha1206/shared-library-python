c97cf0beac48571a544a7b8fc361665fc3393e76
Mamatha Eudulakanti <119730722+Mamatha1206@users.noreply.github.com>
2025-05-27 11:48:40 +0530

    Update pythonBuild.groovy

--- vars/pythonBuild.groovy
+++ vars/pythonBuild.groovy
@@ -2,10 +2,13 @@ def call() {
     stage('Install Dependencies') {
         sh 'pip install -r requirements.txt'
     }
+
     stage('Run Tests') {
-        sh 'pytest tests/'
+        sh 'PYTHONPATH=.:$PYTHONPATH pytest tests/'
     }
+
     stage('Build Package') {
         sh 'python setup.py sdist bdist_wheel'
     }
 }
+
